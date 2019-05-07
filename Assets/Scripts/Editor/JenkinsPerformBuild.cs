d3edefa2ec26bfd3a2aef1145c1d803e4bf1760e
azlobin <T19817321eSt!@#$1>
2019-05-07 12:10:16 +0700

    Update version

---
+++ Assets/Scripts/Editor/JenkinsPerformBuild.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+namespace Editor
+{
+	public class JenkinsPerformBuild 
+	{
+		public static void PerformBuild()
+		{
+			var scenes = new[] {"Assets/Main.unity"};
+			BuildPipeline.BuildPlayer(scenes, "iOSBuild", BuildTarget.iOS, BuildOptions.None);
+		}
+		// Use this for initialization
+		void Start () {
+		
+		}
+	
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
